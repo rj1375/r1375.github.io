33cec390ee2f27c2814894094d81a097dffb574b
rj1375 <rj1375@nyu.edu>
2021-12-23 14:53:39 -0500

    push

---
+++ WebDev-sites/social.js
@@ -0,0 +1,97 @@
+// function reaction() {
+//     var like = document.getElementsByClassName("likeicon")[0];
+
+//     if (likeicon.src == "img/likewhite.png") {
+//         likeicon.src = 
+//     }
+// }
+
+// MOOD BOARD SARAH
+function openSARAHReaction() {
+    document.getElementById("boardSARAH").style.width = "100%";
+    document.getElementById("boardSARAH").style.height = "100%";
+}
+function closeSARAHReaction() {
+    document.getElementById("boardSARAH").style.width = "0";
+    document.getElementById("boardSARAH").style.height = "0";
+}
+
+// MOOD BOARD LINMICK
+function openLINMICKReaction() {
+    document.getElementById("boardLINMICK").style.width = "100%";
+    document.getElementById("boardLINMICK").style.height = "100%";
+}
+function closeLINMICKReaction() {
+    document.getElementById("boardLINMICK").style.width = "0";
+    document.getElementById("boardLINMICK").style.height = "0";
+}
+
+// MOOD BOARD NADIA 1
+function openNADIA1Reaction() {
+    document.getElementById("boardNADIA1").style.width = "100%";
+    document.getElementById("boardNADIA1").style.height = "100%";
+}
+function closeNADIA1Reaction() {
+    document.getElementById("boardNADIA1").style.width = "0";
+    document.getElementById("boardNADIA1").style.height = "0";
+}
+
+// MOOD BOARD NADIA 2
+function openNADIA2Reaction() {
+    document.getElementById("boardNADIA2").style.width = "100%";
+    document.getElementById("boardNADIA2").style.height = "100%";
+}
+function closeNADIA2Reaction() {
+    document.getElementById("boardNADIA2").style.width = "0";
+    document.getElementById("boardNADIA2").style.height = "0";
+}
+
+// MOOD BOARD DEON
+function openDEONReaction() {
+    document.getElementById("boardDEON").style.width = "100%";
+    document.getElementById("boardDEON").style.height = "100%";
+}
+function closeDEONReaction() {
+    document.getElementById("boardDEON").style.width = "0";
+    document.getElementById("boardDEON").style.height = "0";
+}
+
+// MOOD BOARD DANIELLE
+function openDANIELLEReaction() {
+    document.getElementById("boardDANIELLE").style.width = "100%";
+    document.getElementById("boardDANIELLE").style.height = "100%";
+}
+function closeDANIELLEReaction() {
+    document.getElementById("boardDANIELLE").style.width = "0";
+    document.getElementById("boardDANIELLE").style.height = "0";
+}
+
+// MOOD BOARD HUNTER
+function openHUNTERReaction() {
+    document.getElementById("boardHUNTER").style.width = "100%";
+    document.getElementById("boardHUNTER").style.height = "100%";
+}
+function closeHUNTERReaction() {
+    document.getElementById("boardHUNTER").style.width = "0";
+    document.getElementById("boardHUNTER").style.height = "0";
+}
+
+
+
+
+
+
+// PORTFOLIO MENU OPENER
+function openMenu() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+function closeMenu() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
+
+
+// function closeReaction() 
+//     document.getElementById("mood").style.width = "0";
+// } 
